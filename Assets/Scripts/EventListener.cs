3faf5a091800ef1d6ad30e9a06f9db930125822e
gerardo jerez <gerardo.jerez@hotmail.com>
2022-09-21 14:19:38 -0700

    googles de google

---
+++ Assets/Scripts/EventListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    private Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    public void OnEnter()
+    {
+        _renderer.material.color = Color.red;
+
+    }
+
+    public void OnExit()
+    {
+        _renderer.material.color = Color.white;
+    }
+}
